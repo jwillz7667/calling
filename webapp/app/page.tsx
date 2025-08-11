7e4ddc6fe033c24967adb7faf37b4fcfa2806762
jwillz7667 <jwillz7667@gmail.com>
2025-08-11 18:40:44 -0500

    Fix Vercel build - update page version

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,6 +1,6 @@
 import CallInterface from "@/components/call-interface";
 
 export default function Page() {
-  // v1.0.2 - Force redeploy
+  // v1.0.3 - Force redeploy with fixed imports
   return <CallInterface />;
 }
